3d2cb4047ded637c0d540f2d15c6123cf81c6949
Abhinandanlowanshi <abhinandanlowanshi@gmail.com>
2023-07-29 19:41:52 +0530

    integrete google login

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  alert,
 } from 'react-native';
 import AppLogo from '../component/applogo/AppLogo';
 import {hp, RF} from '../common/CommonFunctions';
@@ -25,6 +26,11 @@ import {CommonActions} from '@react-navigation/native';
 import LowOpacityLoader from '../component/LowOpacityLoader';
 import {getAccountImfo} from '../redux/Slice';
 import {useDispatch} from 'react-redux';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
 const Login = ({navigation}) => {
   // const [email, setEmail] = useState('abhinandanlowanshi@gmail.com');
   const [email, setEmail] = useState('');
@@ -37,6 +43,37 @@ const Login = ({navigation}) => {
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
   const [apiError, setApiError] = useState(false);
   const dispatch = useDispatch(dispatch);
+
+  React.useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '523979153716-pq0g7d6sjhc2f7d46nla6ugq4drk7m55.apps.googleusercontent.com',
+      offlineAccess: true,
+    });
+  }, []);
+
+  const GoogleSingUp = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      await GoogleSignin.signIn().then(result => {
+        console.log(result);
+      });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        alert('User cancelled the login flow !');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('Google play services not available or outdated !');
+        // play services not available or outdated
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
   React.useEffect(() => {
     setAuthError('');
     if (validateEmail(email) && password.length >= 6)
@@ -134,9 +171,7 @@ const Login = ({navigation}) => {
           </TouchableOpacity>
           <Text style={style.labelOr}>Or</Text>
           <SocialLoginBt
-            onPress={() => {
-              setApiError('Login with Google will available soon');
-            }}
+            onPress={GoogleSingUp}
             icon={images.googleIcon}
             label={'Login with Google'}
           />
